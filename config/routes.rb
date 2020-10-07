97937d6d8062d877bf7c8d4a788e4ea0437b6697
Kaito-Isa <geraniu6@gmail.com>
2020-10-07 18:01:33 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
-Rails.application.routes.drow do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+Rails.application.routes.draw do
+  root to: 'posts#index'
   post 'posts', to: 'post#create'
 end
