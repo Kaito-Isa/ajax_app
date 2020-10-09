2d25af34536215ce09a32d062c415a1eca280cea
Kaito-Isa <geraniu6@gmail.com>
2020-10-09 14:32:52 +0900

    Update checked.js

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -7,11 +7,7 @@ function check() {
     post.setAttribute("data-load", "true");
     post.addEventListener("click", () => {
       const postId = post.getAttribute("data-id");
- メモ投稿機能実装
-      const XHR = new XMLHttpRequest();　
-
       const XHR = new XMLHttpRequest();
- master
       XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
       XHR.send();
